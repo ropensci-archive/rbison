cf5d7eb232730b63755da99424c561897ff9ee1d
sckott <myrmecocystus@gmail.com>
2014-01-29 09:34:17 -0800

    added package startup message

---
+++ R/startup.r
@@ -0,0 +1,3 @@
+.onAttach <- function(...) {
+  packageStartupMessage("\n\nNew to rbison? Tutorial at http://ropensci.org/tutorials/rbison_tutorial.html \ncitation(package='rbison') for the citation for rbison\nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
+} 
