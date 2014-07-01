a0c618a89fb684b470ed5f80741ee560b176ac25
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-01 11:30:40 -0700

    fixed broken maps test

--- tests/testthat/test-bisonmap.R
+++ tests/testthat/test-bisonmap.R
@@ -7,8 +7,8 @@ map2 <- bisonmap(out, tomap="county")
 map3 <- bisonmap(out, tomap="state")
 
 test_that("bisonmap returns the correct class", {
-  expect_is(map1, c("list"))
-	expect_is(map1$plot, c("ggplot","gg"))
+  expect_is(map1, c("gg"))
+	expect_is(map1, c("ggplot","gg"))
 	expect_that(map2, is_a(c("ggplot","gg")))
 	expect_that(map3, is_a(c("ggplot","gg")))
 })
