b845a91c6fa0baa99bb19637a7201d97539ad3cb
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-18 09:20:11 -0700

    fixed two broken tests

--- tests/testthat/test-bison_solr.R
+++ tests/testthat/test-bison_solr.R
@@ -19,5 +19,5 @@ test_that("bison returns the correct class", {
   expect_that(out_1, is_a("bison_solr"))
   expect_that(out_2, is_a("bison_solr"))
   expect_that(out_3, is_a("bison_solr"))
-  expect_that(out_5_map, is_a(c("gg", "ggplot")))
+  expect_that(out_5_map, is_a("list"))
 })
