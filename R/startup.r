e96ea77d5d14194c5327542230c3b94de52d62c6
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-01 13:11:53 -0700

    removed startup file

--- R/startup.r
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("\n\nNew to rbison? Tutorial at http://ropensci.org/tutorials/rbison_tutorial.html \ncitation(package='rbison') for the citation for rbison\nbison_datause() for data use and bison_citation() for how to cite data from BISON\nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
-} 
