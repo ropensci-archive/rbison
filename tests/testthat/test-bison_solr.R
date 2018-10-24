4ecd7356b52da96e6610a1f8abce2b09daa33769
Scott Chamberlain <myrmecocystus@gmail.com>
2018-10-24 14:30:09 -0700

    preserve bytes for bison_solr

--- tests/testthat/test-bison_solr.R
+++ tests/testthat/test-bison_solr.R
@@ -6,7 +6,7 @@ test_that("bison_solr returns the correct value", {
     expect_that(out_1$facets$facet_queries, equals(NULL))
     expect_is(out_1$points, "data.frame")
     expect_is(out_1, "bison_solr")
-  })
+  }, preserve_exact_body_bytes = TRUE)
 
   vcr::use_cassette("bison_solr_two", {
     out_2 <- bison_solr(scientificName='Ursus americanus', 
@@ -15,19 +15,19 @@ test_that("bison_solr returns the correct value", {
     expect_that(out_2$highlight, equals(NULL))
     expect_that(out_2$points[[1]][1], equals("Ursus americanus"))
     expect_is(out_2, "bison_solr")
-  })
+  }, preserve_exact_body_bytes = TRUE)
 
   vcr::use_cassette("bison_solr_three", {
     out_3 <- bison_solr(scientificName='Ursus americanus', 
                         state_code='New Mexico', rows=50, 
                         fl="occurrence_date,scientificName", verbose = FALSE)
     expect_is(out_3, "bison_solr")
-  })
+  }, preserve_exact_body_bytes = TRUE)
 
   vcr::use_cassette("bison_solr_four", {
     out_4 <- bison_solr()
     expect_that(nrow(out_4$points), equals(10))
-  })
+  }, preserve_exact_body_bytes = TRUE)
 
   vcr::use_cassette("bison_solr_five", {
     out_5 <- bison_solr(scientificName='Helianthus annuus', rows=800, 
@@ -39,7 +39,7 @@ test_that("bison_solr returns the correct value", {
     expect_is(out_5_map$layers, "list")
     expect_named(out_5_map$labels, c("x", "y", "group"))
     expect_is(out_5_map$plot, "environment")
-  })
+  }, preserve_exact_body_bytes = TRUE)
 })
 
 
