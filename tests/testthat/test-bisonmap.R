3b369506c3bde91420d44a0152733d57f63a6e9f
Scott Chamberlain <myrmecocystus@gmail.com>
2020-02-02 15:46:37 -0800

    re-record test fixtures

--- tests/testthat/test-bisonmap.R
+++ tests/testthat/test-bisonmap.R
@@ -2,15 +2,15 @@ context("bisonmap")
 
 test_that("bisonmap returns the correct class", {
   vcr::use_cassette("bisonmap", {
-    out <- bison(species="Aquila chrysaetos", count=10)
+    out <- bison(species="Aquila chrysaetos", count=100)
     map1 <- bisonmap(out)
     map2 <- bisonmap(out, tomap="county")
     map3 <- bisonmap(out, tomap="state")
-    
-    expect_is(map1, "gg")
-    expect_is(map1$data, "data.frame")
-    expect_is(map1$scales, "ScalesList")
-    expect_is(map2, "gg")
-    expect_is(map3, "gg")
-  }, preserve_exact_body_bytes = TRUE)
+  }, preserve_exact_body_bytes = TRUE)  
+
+  expect_is(map1, "gg")
+  expect_is(map1$data, "data.frame")
+  expect_is(map1$scales, "ScalesList")
+  expect_is(map2, "gg")
+  expect_is(map3, "gg")
 })
