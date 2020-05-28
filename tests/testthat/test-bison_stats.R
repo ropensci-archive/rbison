f5ec2d0ad6e39e3433f1791c468ed6c143c0b419
Scott Chamberlain <myrmecocystus@gmail.com>
2020-05-28 15:34:31 -0700

    fix #67 add bison_stats tests

---
+++ tests/testthat/test-bison_stats.R
@@ -0,0 +1,14 @@
+test_that("bison_stats", {
+  vcr::use_cassette("bison_stats", {
+    aa <- bison_stats()
+  })
+    
+  expect_is(aa, "list")
+  expect_named(aa)
+  expect_gt(length(aa), 500)
+  expect_is(aa[[1]], "list")
+  expect_named(aa[[1]])
+  expect_is(aa[[1]]$name, "character")
+  expect_is(aa[[1]]$resources, "character")
+  expect_is(aa[[1]]$data, "data.frame")
+})
