2bf312d35ce070e8b664992464d7476feb81f696
Scott Chamberlain <myrmecocystus@gmail.com>
2020-06-08 10:14:42 -0700

    re-arrange tests so exptectations are outside vcr blocks

--- tests/testthat/test-is.bison.R
+++ tests/testthat/test-is.bison.R
@@ -4,10 +4,10 @@ test_that("is.bison works", {
   vcr::use_cassette("is.bison", {
     out1 <- bison(species="Bison bison", type="scientific_name", count=1)
     out2 <- bison(species="bear", type="common_name", count=1)
-    
-  	expect_is(out1, "bison")
-  	expect_true(is.bison(out1))
-  	expect_is(out2, "bison")
-  	expect_true(is.bison(out2))
   })
+    
+	expect_is(out1, "bison")
+	expect_true(is.bison(out1))
+	expect_is(out2, "bison")
+	expect_true(is.bison(out2))
 })
